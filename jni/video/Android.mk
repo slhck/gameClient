e055e8e09526751f978cd191e8b65c0125a8b00d
Werner Robitza <werner.robitza@gmail.com>
2011-05-13 17:40:36 +0200

    Fixed Android.mk file and added basic Video activity structure.

--- jni/video/Android.mk
+++ jni/video/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := ffmpeg
-# LOCAL_STATIC_LIBRARIES := bambuser-libavcodec bambuser-libavcore bambuser-libavdevice bambuser-libavfilter bambuser-libavformat bambuser-libavutil bambuser-libswscale
+LOCAL_STATIC_LIBRARIES := bambuser-libavcodec bambuser-libavcore bambuser-libavdevice bambuser-libavfilter bambuser-libavformat bambuser-libavutil bambuser-libswscale
 LOCAL_SRC_FILES := at_ac_univie_gameclient_video_VideoGLSurfaceView.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../bambuser_ffmpeg/
 LOCAL_LDLIBS    := -L$(LOCAL_PATH)/../bambuser_ffmpeg -lavcodec -lavcore -lavdevice -lavfilter -lavformat -lavutil -lswscale -llog -lz -lGLESv1_CM
--- src/at/ac/univie/gameclient/Start.java
+++ src/at/ac/univie/gameclient/Start.java
@@ -1,13 +1,21 @@
 package at.ac.univie.gameclient;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Start extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);       
+	}
+
+	public void onResume() {
+		super.onResume();
+		Intent intent = new Intent();
+		intent.setClass(getApplicationContext(), at.ac.univie.gameclient.video.VideoActivity.class);
+		startActivity(intent);
+	}
 }
